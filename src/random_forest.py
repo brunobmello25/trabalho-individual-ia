b4a600f36b656675cd64f29525edd8f95bd9195c
Bruno Mello <bruno.barros.mello@gmail.com>
2025-01-09 14:35:44 -0300

    refactor random forest

--- src/random_forest.py
+++ src/random_forest.py
@@ -18,18 +18,33 @@ class RandomForest:
     def run(self):
         features = self.dataset.data.features
         targets = self.dataset.data.targets
-        print(targets.shape)
         # Necessário para converter o shape de (n, 1) para (n,) (matriz de uma coluna pra vetor)
         targets = targets.values.ravel()
-        print(targets.shape)
 
+        encoded_features = self._onehotencode(features)
+
+        features_train, features_test, target_train, target_test = self._split(
+            encoded_features, targets)
+
+        classifier = self._make_classifier()
+
+        self._train_classifier(classifier, features_train, target_train)
+
+        prediction = self._predict(classifier, features_test)
+
+        accuracy = accuracy_score(target_test, prediction)
+        print(f"Precisão da Random Forest: {accuracy:.2f}")
+
+    def _onehotencode(self, features):
         encoder = OneHotEncoder()
         before = time.time()
         encoded_features = encoder.fit_transform(features)
         after = time.time()
         duration = Utils.format_duration(before, after)
         print(f"tempo para encode: {duration}ms")
+        return encoded_features
 
+    def _split(self, encoded_features, targets):
         before = time.time()
         features_train, features_test, target_train, target_test = train_test_split(
             encoded_features, targets, test_size=0.3, random_state=self.random_state)
@@ -37,8 +52,14 @@ class RandomForest:
         duration = Utils.format_duration(before, after)
         print(f"tempo para split: {duration}ms")
 
+        return features_train, features_test, target_train, target_test
+
+    def _make_classifier(self):
         classifier = RandomForestClassifier(
             random_state=self.random_state, n_estimators=self.tree_count)
+        return classifier
+
+    def _train_classifier(self, classifier, features_train, target_train):
         before = time.time()
         classifier.fit(features_train, target_train)
         after = time.time()
@@ -46,11 +67,10 @@ class RandomForest:
         print(
             f"tempo para treinar com {self.tree_count} árvores: {duration}ms")
 
+    def _predict(self, classifier, features_test):
         before = time.time()
         prediction = classifier.predict(features_test)
         after = time.time()
         duration = Utils.format_duration(before, after)
         print(f"tempo para predict: {duration}ms")
-
-        accuracy = accuracy_score(target_test, prediction)
-        print(f"Precisão da Random Forest: {accuracy:.2f}")
+        return prediction
