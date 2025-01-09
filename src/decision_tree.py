34ebbf75be0ab765eced299f1aa190c50994bf45
Bruno Mello <bruno.barros.mello@gmail.com>
2025-01-09 14:01:00 -0300

    print parameters and plot tree on decision_tree

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -2,8 +2,9 @@ import time
 from typing import Any
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
 
 import src.utils as Utils
 
@@ -31,7 +32,17 @@ class DecisionTree:
         duration = Utils.format_duration(before, after)
         print(f"tempo para split: {duration}ms")
 
-        classifier = DecisionTreeClassifier(random_state=self.random_state)
+        classifier = DecisionTreeClassifier(
+            random_state=self.random_state, criterion='gini')
+        print('---------------------')
+        print('parameters:')
+        print('criterion: ', classifier.criterion)
+        print('max_depth: ', classifier.max_depth)
+        print('min_samples_split: ', classifier.min_samples_split)
+        print('min_samples_leaf: ', classifier.min_samples_leaf)
+        print('max_features: ', classifier.max_features)
+        print('class_weight: ', classifier.class_weight)
+        print('---------------------')
         before = time.time()
         classifier.fit(features_train, target_train)
         after = time.time()
@@ -46,3 +57,10 @@ class DecisionTree:
 
         accuracy = accuracy_score(target_test, prediction)
         print(f"Precisão da Decision Tree: {accuracy:.2f}")
+
+        plt.figure(figsize=(80, 40))
+        plot_tree(classifier, filled=True,
+                  feature_names=encoder.get_feature_names_out(), class_names=True)
+        plt.title("Árvore de Decisão")
+        plt.savefig("decision_tree.png")  # Salva a imagem no formato PNG
+        plt.close()
