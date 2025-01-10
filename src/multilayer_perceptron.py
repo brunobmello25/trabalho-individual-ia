7142a54b867f44aeddfe260238c4b7a7c14f8dd7
Bruno Mello <bruno.barros.mello@gmail.com>
2025-01-10 15:47:35 -0300

    wrap up

--- src/multilayer_perceptron.py
+++ src/multilayer_perceptron.py
@@ -1,6 +1,6 @@
 import time
 from typing import Any, List, Tuple
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.neural_network import MLPClassifier
@@ -15,6 +15,9 @@ class MultilayerPerceptron:
         self.hidden_layer_configurations = self.generate_layer_configurations(
             max_neurons=350, max_layers=5, step=50)
         self.best_accuracy = -1
+        self.best_f1_score = -1
+        self.best_accuracy_config = None
+        self.best_f1_score_config = None
 
     def generate_layer_configurations(self, max_neurons: int, max_layers: int, step: int = 10) -> List[Tuple[int, ...]]:
         neuron_counts = list(range(step, max_neurons + 1, step))
@@ -43,28 +46,26 @@ class MultilayerPerceptron:
                 f"Testando configuração {i+1}/{len(self.hidden_layer_configurations)} de camadas ocultas: {hidden_layers}")
             i += 1
             classifier = self._make_classifier(hidden_layers)
-            accuracy, overfitting_chance = self._train_and_evaluate(
+            accuracy, f1 = self._train_and_evaluate(
                 classifier, features_train, target_train, features_test, target_test)
             if accuracy > self.best_accuracy:
                 self.best_accuracy = accuracy
-                self.best_configuration = hidden_layers
-            results.append((hidden_layers, accuracy, overfitting_chance))
+                self.best_accuracy_config = hidden_layers
+            if f1 > self.best_f1_score:
+                self.best_f1_score = f1
+                self.best_f1_score_config = hidden_layers
+            results.append((hidden_layers, accuracy, f1))
             print('--------------------')
 
         # Exibir resultados
         print("\nResultados:")
-        for config, accuracy, overfitting in results:
+        for config, accuracy, f1 in results:
             print(
-                f"Camadas ocultas: {config}, Precisão: {accuracy:.4f}, Chance de Overfitting: {overfitting:.2f}")
-        print('melhor configuração: ', self.best_configuration)
-        print('melhor acurácia: ', self.best_accuracy)
-
-    def _measure_overfitting_chance(self, classifier, features_train, target_train, features_test, target_test):
-        train_accuracy = accuracy_score(
-            target_train, classifier.predict(features_train))
-        target_accuracy = accuracy_score(
-            target_test, classifier.predict(features_test))
-        return abs(target_accuracy - train_accuracy)
+                f"Camadas ocultas: {config}, Precisão: {accuracy:.4f}, F1-score: {f1:.4f}")
+        print('Melhor configuração pela acurácia: ', self.best_accuracy_config)
+        print('Melhor acurácia: ', self.best_accuracy)
+        print('Melhor configuração pela F1-score: ', self.best_f1_score_config)
+        print('Melhor F1-score: ', self.best_f1_score)
 
     def _onehotencode(self, features):
         encoder = OneHotEncoder()
@@ -96,24 +97,18 @@ class MultilayerPerceptron:
         self._train_classifier(classifier, features_train, target_train)
         prediction = self._predict(classifier, features_test)
         accuracy = accuracy_score(target_test, prediction)
-        # print(f"Precisão da Multilayer Perceptron: {accuracy:.4f}")
-        overfitting_chance = self._measure_overfitting_chance(
-            classifier, features_train, target_train, features_test, target_test)
-        # print(f"Chance de overfitting: {overfitting_chance:.2f}")
-        return accuracy, overfitting_chance
+        f1 = f1_score(target_test, prediction, average='weighted')
+        return accuracy, f1
 
     def _train_classifier(self, classifier, features_train, target_train):
         before = time.time()
         classifier.fit(features_train, target_train)
         after = time.time()
         duration = Utils.format_duration(before, after)
-        # print(
-        #     f"tempo para treinar com layers {classifier.hidden_layer_sizes}: {duration}ms")
 
     def _predict(self, classifier, features_test):
         before = time.time()
         prediction = classifier.predict(features_test)
         after = time.time()
         duration = Utils.format_duration(before, after)
-        # print(f"tempo para predict: {duration}ms")
         return prediction
